386689430c5696ae5f3a85f20403fa9c3af370d5
Umang Patel <patelumang087@gmail.com>
2026-02-06 23:46:14 +0530

    rotes defined

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import toast, { Toaster } from "react-hot-toast"
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import BlogLandingPage from './pages/Blog/components/BlogLandingPage'
+import BlogPostView from './pages/Blog/components/BlogPostView'
+import SearchPosts from './pages/Blog/components/SearchPosts'
+import AdminLogin from './pages/Admin/components/AdminLogin'
+import PostByTags from './pages/Blog/components/PostByTags'
+import BlogPosts from './pages/Admin/components/BlogPosts'
+import BlogPostEditor from './pages/Admin/components/BlogPostEditor'
+import Comments from './pages/Admin/components/Comments'
+import Dashboard from './pages/Admin/components/Dashboard'
 
 const App = () => {
   return (
-    <div className='bg-red-500'>
-      App
+    <div className=''>
+      <Router>
+        <Routes>
+
+        <Route path="/" element={<BlogLandingPage />}/>
+        <Route path="/:slug" element={<BlogPostView />}/>
+        <Route path="/tag/:tagName" element={<PostByTags />}/>
+        <Route path='/search' element={<SearchPosts />}/>
+
+        <Route path='/admin-login' element={<AdminLogin />} />
+
+        <Route>
+          <Route path='/admin/dashboard' element={<Dashboard />}/>
+          <Route path='/admin/posts' element={<BlogPosts />}/>
+          <Route path='/admin/create' element={<BlogPostEditor />} />
+          <Route 
+          path='/admin/edit/:postSlug'
+          element={<BlogPostEditor isEdit={true} />}
+          />
+          <Route path='/admin/comments' element={<Comments />} />
+
+          <Route />
+        </Route>
+
+        </Routes>
+      </Router>
     </div>
   )
 }
